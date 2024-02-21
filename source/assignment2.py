4f215fcc87dbfa21cfaec811b961aa43210c132c
vanshita-tilwani <msvtilwani@gmail.com>
2024-02-21 11:10:26 -0500

    adding code comments

--- source/assignment2.py
+++ source/assignment2.py
@@ -3,30 +3,54 @@ from util import  StemData, ProcessQueries
 from indexer import GenerateIndexes
 from constants import Constants
 from retrievalmodel import Model
+import os
 
 def __main__() :
+    # Reading documents and stopwords from resources
     documents = readDocuments()
     stopwords = readStopwords()
+    # Reading the indexes if already exists, if the indexes are not present then generating new indexes
     document_mapping, unstemmed_index, stemmed_index = GenerateIndexes(documents, stopwords)
 
+    # Reading queries from the resources
     queries = readQueries()
+    # Processing the queries, i.e. tokenizing and stopword removal
     processedQueries = ProcessQueries(queries, stopwords)
+    # Stemming the queries
     stemmedQueries = StemData(processedQueries)
-    models = [Constants.OKAPI_TF, Constants.TF_IDF, Constants.BM_25, Constants.LM_LAPLACE, Constants.LM_JELINEKMERCER]
+
+    # Models to run retrieval models on
+    models = [Constants.TF_IDF, Constants.BM_25, Constants.LM_JELINEKMERCER]
+    # Executing Queries for the models on unstemmed index
     __executeQueries(Constants.INDEX_TYPE_UNSTEMMED, processedQueries, models, document_mapping, unstemmed_index)
+    # Executing Queries for the models on stemmed index
     __executeQueries(Constants.INDEX_TYPE_STEMMED, stemmedQueries, models, document_mapping, stemmed_index)
     print('Inverted index is generated for both stemmed and unstemmed documents')
 
 def __executeQueries(index_type, queries, models, document_mapping, index) :
+    # Deleting result file before running queries
+    for model in models :
+        __deleteResultFiles(index_type, model)
+
+    # Creating the model object with index and documents 
     retrieval_model = Model(index, document_mapping)
     for query in queries:
         for model in models:
+            # Setting model type to use to run queries
             retrieval_model.SetModel(model)
+            # Setting the query which will be used to run on the model
             retrieval_model.SetQuery(queries[query])
+            # Executing query on the model
             __executeQuery(index_type, retrieval_model, document_mapping, query,  model)
 
 def __executeQuery(index_type, retrieval_model, document_mapping, query, model) :
+    # Execute the query on the model using the index
     score = retrieval_model.Execute()
     WriteToResults(index_type, model, query, score, document_mapping)
+
+def __deleteResultFiles(index_type, model) :
+    file_path =  Constants.OUTPUT_PATH + '/' + index_type + '/' + model + '.txt'
+    if(os.path.exists(file_path)):
+        os.remove(file_path)
 
 __main__()
